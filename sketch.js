c3f2d418b2cdc0ad1233f7694d9d9293d8963f85
Frederik Shull <frederik.shull@web.de>
2018-10-06 18:56:27 +0200

    Some controls added.

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
-const scl = 80;
+var scl = 80;
+var fullwidth = false;
 const spc = 10;
 var showdebug = 5;
 var
@@ -36,17 +37,38 @@ function draw() {
 		}
 	}
 	fps.html(frameRate().toFixed(1));
-	if (circles.left[1].rotation.toFixed(1) == -(TAU.toFixed(1) * 2)) {
+	if (circles.left[0].rotation.toFixed(1) == -TAU.toFixed(1)) {
 		reset();
 	}
 }
 
 function keyPressed() {
-	if (key == " ") {
-		showdebug += 1;
-		if (showdebug > 4) {
-			showdebug = 0;
-		}
+	switch (key) {
+		case " ":
+			showdebug += 1;
+			if (showdebug > 4) {
+				showdebug = 0;
+			}
+			break;
+		case "Control":
+			reset();
+			break;
+		case "ArrowUp":
+			scl -= 10;
+			reset();
+			break;
+		case "ArrowDown":
+			scl += 10;
+			reset();
+			break;
+		case "ArrowLeft":
+			fullwidth = false;
+			reset();
+			break;
+		case "ArrowRight":
+			fullwidth = true;
+			reset();
+			break;
 	}
 }
 
@@ -56,15 +78,19 @@ function reset() {
 		"top": []
 	};
 	curves = [];
+	let width2 = width;
+	if (fullwidth == false) {
+		width2 = height;
+	}
 	for (let i = 1; i < floor(height / scl); i++) {
 		circles.left.push(new Circle("left", i));
 	}
-	for (let i = 1; i < floor(width / scl); i++) {
+	for (let i = 1; i < floor(width2 / scl); i++) {
 		circles.top.push(new Circle("top", i));
 	}
 	for (let i = 1; i < floor(height / scl); i++) {
 		curves.push([]);
-		for (let j = 1; j < floor(width / scl); j++) {
+		for (let j = 1; j < floor(width2 / scl); j++) {
 			curves[i - 1].push(new Curve(j * scl, i * scl));
 		}
 	}
